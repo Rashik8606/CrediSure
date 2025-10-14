09d0ae9f0b98a37d00cf248bf6d241d819b0f15c
rashik <mhdrashik112@gmail.com>
2025-10-14 11:46:53 +0530

    Some Changes Models

--- backend/credisure/loans/models.py
+++ backend/credisure/loans/models.py
@@ -11,12 +11,18 @@ class loanRequestForm(models.Model):
         (BORROWER, 'borrower'),
         (ADMIN, 'admin'),
     ]
-    
+
+    STATUS_CHOISES = [
+        ('pending','pending'),
+        ('approved','approved'),
+        ('rejected','rejected'),
+    ]
+
     borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='loans')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     purpose = models.CharField(max_length=200)
     duration_months = models.IntegerField()
-    status = models.CharField(max_length=20, default='pending')
+    status = models.CharField(max_length=20,choices=STATUS_CHOISES, default='pending')
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
