3eb35265b3f386e99ab3e5981a07ffdf4c3436f2
rashik <mhdrashik112@gmail.com>
2026-04-10 11:30:03 +0530

    Some Correction Backend

---
+++ backend/payment-service/payment_service/payments/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import CreateEmiPaymentView, VerifyEmiPaymentsView
+
+
+
+urlpatterns = [
+    path('emi/create-payment/',CreateEmiPaymentView.as_view()),
+    path('emi/verify-payment/',VerifyEmiPaymentsView.as_view()),
+]
